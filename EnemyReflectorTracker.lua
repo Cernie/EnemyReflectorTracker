d6711a7a5d7d5024cca1ab13608fc849f923ab7a
Cernie <33034149+Cernie@users.noreply.github.com>
2025-05-27 20:21:00 -0500

    inital commit v1.0

---
+++ EnemyReflectorTracker.lua
@@ -0,0 +1,73 @@
+ReflectorTracker = {}
+
+function ReflectorTracker_OnLoad()
+	this:RegisterEvent("CHAT_MSG_SPELL_HOSTILEPLAYER_BUFF")
+	this:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_HOSTILEPLAYER_BUFFS")
+	this:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_CREATURE_BUFFS")
+	-- Create frame for handling events
+	ReflectorTrackerFrame = CreateFrame("Frame")
+	-- Frame to show the reflector icon
+	iconFrame = CreateFrame("Frame", nil, UIParent)
+	iconFrame:SetWidth(64)
+	iconFrame:SetHeight(64)
+	iconFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 200)
+	iconFrame:Hide()
+	-- Frame to show the icon texture
+	iconTexture = iconFrame:CreateTexture(nil, "OVERLAY")
+	iconTexture:SetAllPoints()
+	iconTexture:SetTexture("")  -- will set dynamically
+
+	-- Name text above icon
+	nameText = iconFrame:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
+	nameText:SetPoint("BOTTOM", iconFrame, "TOP", 0, 4)
+	nameText:SetText("")
+	
+	-- Countdown timer
+	iconFrame:SetScript("OnUpdate", function()
+		local elapsed = GetTime() - iconFrame_startTime
+		if elapsed >= 5 then
+			iconFrame:Hide()
+			nameText:SetText("")
+		end
+	end)
+	DEFAULT_CHAT_FRAME:AddMessage("EnemyReflectorTracker loaded.")
+end
+
+function ReflectorTracker_OnEvent(event, arg1, arg2, arg3)
+	if (event == "CHAT_MSG_SPELL_HOSTILEPLAYER_BUFF" or event == "CHAT_MSG_SPELL_PERIODIC_HOSTILEPLAYER_BUFFS") then
+		for name, data in pairs(reflectorSpells) do
+			local playerNameIndex = string.find(arg1, ""..data.effectText)
+			local playerName = nil;
+			if(playerNameIndex ~= nil) then
+				playerName = string.sub(arg1, 1, playerNameIndex - 1)
+			end
+			if playerName then
+				iconTexture:SetTexture(data.texture)
+				nameText:SetText(playerName)
+				iconFrame_Show()
+			end
+		end
+	end
+end
+
+-- Reflector spell effect lookup
+reflectorSpells = {
+    ["Hyper-Radiant Flame Reflector"] = {
+        effectText = "gains Fire Reflector", -- what shows in combat log
+        texture = "Interface\\Icons\\Spell_Fire_FireArmor"
+    },
+    ["Gyrofreeze Ice Reflector"] = {
+        effectText = "gains Frost Reflector",
+        texture = "Interface\\Icons\\Spell_Frost_FrostWard"
+    },
+    ["Ultra-Flash Shadow Reflector"] = {
+        effectText = "gains Shadow Reflector",
+        texture = "Interface\\Icons\\Spell_Shadow_AntiShadow"
+    }
+}
+
+--IconFrame Show
+function iconFrame_Show()
+	iconFrame_startTime = GetTime()
+	iconFrame:Show()
+end
